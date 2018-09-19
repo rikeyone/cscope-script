0084c5b0581a7919bd1fa3f6153ba349b28bd12b
rikeyone <xiehaocheng127@163.com>
2018-09-19 11:47:18 +0800

    [shell] initial version of cscope script
    Change-Id: Ie93d9627528ba016b43b25bf7dbb26c0485498fa
    Signed-off-by: rikeyone <xiehaocheng127@163.com>

---
+++ cscope.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+SRC=`pwd`
+OUT=${SRC}
+update=1
+change=0
+while getopts "o:d:uc" opt;
+do
+    case $opt in
+        d)
+			SRC=$(cd "$(dirname "$OPTARG")" && pwd)/$(basename "$OPTARG")
+			;;
+        o)
+            OUT=$OPTARG
+			;;
+		u)
+			update=1
+			;;
+		c)
+			change=1
+			;;
+		?)
+			echo "invaild option!"
+			exit 1
+	esac
+done
+
+echo "project source:${SRC}"
+echo "database output:${OUT}"
+
+cd ${OUT}
+
+if [ 1 -eq ${change} ]; then
+	echo "change project cscope database!"
+	res=$(find ${SRC} -maxdepth 1 -name cscope.out)
+	if [ "x"${res} = "x" ]; then
+		res=$(find ${SRC} -maxdepth 4 -name cscope.out)
+		if [ "x"${res} != "x" ]; then
+			echo "Found cscope database:${res}, just change CSCOPE_DB env!"
+			export CSCOPE_DB=${res}
+		fi
+		echo "Not found cscope database, generate cscope database!"
+		find ${SRC} -name "*.h" -o -name "*.c" -o -name "*.cc" > cscope.files
+		cscope -bkq -i cscope.files 
+		#ctags -R *
+		export CSCOPE_DB=${OUT}/cscope.out
+	else
+		echo "Found cscope database:${res}, just change CSCOPE_DB env!"
+		export CSCOPE_DB=${res}
+	fi
+elif [ 1 -eq ${update} ]; then
+	echo "udpate project cscope database!"
+	find ${SRC} -name "*.h" -o -name "*.c" -o -name "*.cc" > cscope.files
+	cscope -bkq -i cscope.files
+	#ctags -R *
+	export CSCOPE_DB=${OUT}/cscope.out
+fi
+
+echo CSCOPE_DB_PATH=${CSCOPE_DB}
+
