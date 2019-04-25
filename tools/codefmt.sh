a7cbc7f538dd4a737ce421874a7b732b656290b0
rikeyone <xiehaocheng127@163.com>
2019-04-25 17:39:24 +0800

    Add source code format script tool.
    A script tool which would do two things:
    1.change a source code file from dos to unix file format
    2.delete unused space symbol at the tail of each line
    
    Signed-off-by: rikeyone <xiehaocheng127@163.com>

---
+++ tools/codefmt.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ x$1 = x ]; then
+	echo "Please pass an arg as source code path!!!\n"
+	exit 1
+fi
+
+SRC=$(find $1 -name "*.c" -o -name "*.h")
+
+echo "Handle Source Files:"
+echo "${SRC}"
+
+for file in ${SRC}
+do
+sed -i -e 's/\r$//' $file
+sed -i -e 's/\s*$//' $file
+done
