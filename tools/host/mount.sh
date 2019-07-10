f8cb5410b5dc1dfc9dedbad89548b9cc055192ee
rikeyone <xiehaocheng127@163.com>
2019-07-10 11:19:48 +0800

    Add nfs & cifs support.
    Signed-off-by: rikeyone <xiehaocheng127@163.com>

--- tools/host/mount.sh
+++ tools/host/mount.sh
@@ -1,4 +1,5 @@
+sudo apt-get install nfs-common
 sudo apt-get install cifs-utils
-sudo mount -t cifs -o username="xhc",password="123456",gid="1000",uid="1000",iocharset=utf8,rw,dir_mode=0777,file_mode=0777 //172.20.20.20/share  /media/cloud/bsplogshare/
-sudo mount -t ntfs -o gid="1000",uid="1000",iocharset=utf8,rw,dir_mode=0666,file_mode=0777 /dev/sdf1  /media/xiehaocheng/seagate
+
 sudo mount -t cifs -o username="xhc",password="123456",domain="MEIZU",rw,uid=1000,gid=1000 //dailybuild2/firmware/DailyBuild4Test /media/cloud/dailybuild2/
+sudo mount -t ntfs -o gid="1000",uid="1000",iocharset=utf8,rw,dir_mode=0666,file_mode=0777 /dev/sdf1  /media/xiehaocheng/seagate
